bc99935ac01db27926462496cab83bd7616f44ed
home <te@email.com>
2020-12-13 23:42:41 +0100

    get video duration using ffmpeg and display the result

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+let win;
 
 app.on('ready',  () => {
-  const win = new BrowserWindow({});
+    win = new BrowserWindow( {
+    webPreferences: { nodeIntegration: true }
+  });
   win.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log(metadata.format);
+    win.webContents.send('video:metadata', metadata.format.duration);
+  })
 });
